10a796458a945af48891c856964b00649321018a
trijuhari <trijuhaari0@gmail.com>
2021-04-29 11:29:18 +0700

    binarygap

---
+++ binary_gap.py
@@ -0,0 +1,16 @@
+def solution(n):
+    bin_rep = str(bin(n))[2:]
+    bin_gap = False
+    bin_max = 0 
+    bin_counter = 0 
+    for symbol in bin_rep:
+        if symbol == "1":
+            if bin_max < bin_counter:
+                bin_max = bin_counter
+            bin_gap = True
+            bin_counter = 0
+        elif bin_gap :
+            bin_counter += 1
+    return bin_max
+var1 =  solution(1041)
+print(var1)
