cdd466bc8556d577f03409615cec3674b6b1ed8e
trijuhari <trijuhaari0@gmail.com>
2021-04-29 10:40:48 +0700

    add fizzbuzz

---
+++ fizzbuzz.py
@@ -0,0 +1,12 @@
+a = []
+for i in range(1,100):
+    if  i % 3 ==0 and i % 5 == 0 :
+        a.append("fizzbuzz")
+    if  i % 3 == 0 :
+        a.append("fizz")
+    elif  i% 5 ==0 :
+        a.append("buzz")
+    else:
+        a.append(i)
+        
+print(a)
